c0a9d51ec9a19b2179ceed8c19aeab3273b50a3c
Jonas Schnelli <dev@jonasschnelli.ch>
2016-08-12 13:04:44 +0200

    Add mempool statistics collector

--- src/init.cpp
+++ src/init.cpp
@@ -34,6 +34,7 @@
 #include "script/standard.h"
 #include "script/sigcache.h"
 #include "scheduler.h"
+#include "stats/stats.h"
 #include "timedata.h"
 #include "txdb.h"
 #include "txmempool.h"
@@ -516,6 +517,7 @@ std::string HelpMessage(HelpMessageMode mode)
         strUsage += HelpMessageOpt("-rpcservertimeout=<n>", strprintf("Timeout during HTTP requests (default: %d)", DEFAULT_HTTP_SERVER_TIMEOUT));
     }
 
+    strUsage += CStats::getHelpString(showDebug);
     return strUsage;
 }
 
@@ -1132,6 +1134,10 @@ bool AppInitParameterInteraction()
             }
         }
     }
+
+    if (!CStats::parameterInteraction())
+        return false;
+
     return true;
 }
 
@@ -1701,6 +1707,8 @@ bool AppInitMain(boost::thread_group& threadGroup, CScheduler& scheduler)
         pwallet->postInitProcess(scheduler);
     }
 #endif
+
+    CStats::DefaultStats()->startCollecting(scheduler);
 
     return !fRequestShutdown;
 }
