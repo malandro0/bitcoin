375d6322e63a10aedaf159aed8d7fd8138ff1b66
Luke Dashjr <luke-jr+git@utopios.org>
2018-03-12 17:59:50 +0000

    RPC: Add ancestor{count,size,fees} to listunspent output

--- src/wallet/rpcwallet.cpp
+++ src/wallet/rpcwallet.cpp
@@ -26,6 +26,7 @@
 #include <script/sign.h>
 #include <shutdown.h>
 #include <timedata.h>
+#include <txmempool.h>
 #include <util/system.h>
 #include <util/moneystr.h>
 #include <wallet/coincontrol.h>
@@ -2778,6 +2779,9 @@ static UniValue listunspent(const JSONRPCRequest& request)
             "    \"scriptPubKey\" : \"key\",   (string) the script key\n"
             "    \"amount\" : x.xxx,         (numeric) the transaction output amount in " + CURRENCY_UNIT + "\n"
             "    \"confirmations\" : n,      (numeric) The number of confirmations\n"
+            "    \"ancestorcount\" : n,      (numeric) The number of unconfirmed ancestors if spent\n"
+            "    \"ancestorsize\" : n,       (numeric) The virtual transaction size of all unconfirmed ancestors\n"
+            "    \"ancestorfees\" : n,       (numeric) The modified fees of all unconfirmed ancestors\n"
             "    \"redeemScript\" : n        (string) The redeemScript if scriptPubKey is P2SH\n"
             "    \"spendable\" : xxx,        (bool) Whether we have the private keys to spend this output\n"
             "    \"solvable\" : xxx,         (bool) Whether we know how to spend this output, ignoring the lack of keys\n"
@@ -2899,6 +2903,15 @@ static UniValue listunspent(const JSONRPCRequest& request)
         entry.pushKV("scriptPubKey", HexStr(scriptPubKey.begin(), scriptPubKey.end()));
         entry.pushKV("amount", ValueFromAmount(out.tx->tx->vout[out.i].nValue));
         entry.pushKV("confirmations", out.nDepth);
+        if (!out.nDepth) {
+            LOCK(mempool.cs);
+            const auto it = mempool.mapTx.find(out.tx->GetHash());
+            if (it != mempool.mapTx.end()) {
+                entry.pushKV("ancestorcount", it->GetCountWithAncestors());
+                entry.pushKV("ancestorsize", it->GetSizeWithAncestors());
+                entry.pushKV("ancestorfees", it->GetModFeesWithAncestors());
+            }
+        }
         entry.pushKV("spendable", out.fSpendable);
         entry.pushKV("solvable", out.fSolvable);
         if (out.fSolvable) {
