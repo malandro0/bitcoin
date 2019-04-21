9081dcef9861486996b4e334f5f76b47b922525a
Luke Dashjr <luke-jr+git@utopios.org>
2019-04-21 03:00:52 +0000

    [POLICY] Policy-accept sending to future native witness outputs with -sendtofuture=1
    Based on c634b1e2076d8e15a8284638475e26c691d4e100 by Pieter Wuille

--- src/init.cpp
+++ src/init.cpp
@@ -518,6 +518,7 @@ void SetupServerArgs()
     gArgs.AddArg("-mempoolreplacement", strprintf("Enable transaction replacement in the memory pool (default: %u)", DEFAULT_ENABLE_REPLACEMENT), false, OptionsCategory::NODE_RELAY);
     gArgs.AddArg("-minrelaytxfee=<amt>", strprintf("Fees (in %s/kB) smaller than this are considered zero fee for relaying, mining and transaction creation (default: %s)",
         CURRENCY_UNIT, FormatMoney(DEFAULT_MIN_RELAY_TX_FEE)), false, OptionsCategory::NODE_RELAY);
+    gArgs.AddArg("-sendtofuture", strprintf("Relay and mine transactions sending to future witness script versions (default: %u)", DEFAULT_ALLOW_SENDING_TO_FUTURE_WITNESS_VERSIONS), false, OptionsCategory::NODE_RELAY);
     gArgs.AddArg("-whitelistforcerelay", strprintf("Force relay of transactions from whitelisted peers even if they violate local relay policy (default: %d)", DEFAULT_WHITELISTFORCERELAY), false, OptionsCategory::NODE_RELAY);
     gArgs.AddArg("-whitelistrelay", strprintf("Accept relayed transactions received from whitelisted peers even when not relaying transactions (default: %d)", DEFAULT_WHITELISTRELAY), false, OptionsCategory::NODE_RELAY);
 
@@ -1147,6 +1148,8 @@ bool AppInitParameterInteraction()
     fIsBareMultisigStd = gArgs.GetBoolArg("-permitbaremultisig", DEFAULT_PERMIT_BAREMULTISIG);
     fAcceptDatacarrier = gArgs.GetBoolArg("-datacarrier", DEFAULT_ACCEPT_DATACARRIER);
     nMaxDatacarrierBytes = gArgs.GetArg("-datacarriersize", nMaxDatacarrierBytes);
+
+    g_allow_sending_to_future_witness_versions = gArgs.GetBoolArg("-sendtofuture", DEFAULT_ALLOW_SENDING_TO_FUTURE_WITNESS_VERSIONS);
 
     // Option to startup with mocktime set (used for regression testing):
     SetMockTime(gArgs.GetArg("-mocktime", 0)); // SetMockTime(0) is a no-op
