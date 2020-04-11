bitcoincore-dev/bitcoin-2001-863168-294116-d812cf1189

Entirely disable BIP157 support without -peercfilters option

src/net_processing.cpp
@@ -1277,9 +1277,11 @@ void PeerLogicValidation::UpdatedBlockTip(const CBlockIndex *pindexNew, const CB
         connman->WakeMessageHandler();
     }
 
-    ForEachBlockFilterIndex(
-        [](BlockFilterIndex& index) { UpdateCFHeadersCache(index); }
-    );
+    if (gArgs.GetBoolArg("-peercfilters", DEFAULT_PEERCFILTERS)) {
+        ForEachBlockFilterIndex(
+            [](BlockFilterIndex& index) { UpdateCFHeadersCache(index); }
+        );
+    }
 }
 
 /**
@@ -3493,14 +3495,16 @@ bool static ProcessMessage(CNode* pfrom, const std::string& strCommand, CDataStr
         return true;
     }
 
-    if (strCommand == NetMsgType::GETCFILTERS) {
-        return ProcessGetCFilters(pfrom, vRecv, chainparams, connman);
-    }
-    if (strCommand == NetMsgType::GETCFHEADERS) {
-        return ProcessGetCFHeaders(pfrom, vRecv, chainparams, connman);
-    }
-    if (strCommand == NetMsgType::GETCFCHECKPT) {
-        return ProcessGetCFCheckPt(pfrom, vRecv, chainparams, connman);
+    if (pfrom->GetLocalServices() & NODE_COMPACT_FILTERS) {
+        if (strCommand == NetMsgType::GETCFILTERS) {
+            return ProcessGetCFilters(pfrom, vRecv, chainparams, connman);
+        }
+        if (strCommand == NetMsgType::GETCFHEADERS) {
+            return ProcessGetCFHeaders(pfrom, vRecv, chainparams, connman);
+        }
+        if (strCommand == NetMsgType::GETCFCHECKPT) {
+            return ProcessGetCFCheckPt(pfrom, vRecv, chainparams, connman);
+        }
     }
 
     if (strCommand == NetMsgType::NOTFOUND) {

malandro0/bitcoin

src/net_processing.cpp
@@ -3464,14 +3464,16 @@ bool ProcessMessage(CNode* pfrom, const std::string& msg_type, CDataStream& vRec
         return true;
     }
 
-    if (msg_type == NetMsgType::GETCFILTERS) {
-        return ProcessGetCFilters(pfrom, vRecv, chainparams, connman);
-    }
-    if (msg_type == NetMsgType::GETCFHEADERS) {
-        return ProcessGetCFHeaders(pfrom, vRecv, chainparams, connman);
-    }
-    if (msg_type == NetMsgType::GETCFCHECKPT) {
-        return ProcessGetCFCheckPt(pfrom, vRecv, chainparams, connman);
+    if (pfrom->GetLocalServices() & NODE_COMPACT_FILTERS) {
+        if (msg_type == NetMsgType::GETCFILTERS) {
+            return ProcessGetCFilters(pfrom, vRecv, chainparams, connman);
+        }
+        if (msg_type == NetMsgType::GETCFHEADERS) {
+            return ProcessGetCFHeaders(pfrom, vRecv, chainparams, connman);
+        }
+        if (msg_type == NetMsgType::GETCFCHECKPT) {
+            return ProcessGetCFCheckPt(pfrom, vRecv, chainparams, connman);
+        }
     }
 
     if (msg_type == NetMsgType::NOTFOUND) {