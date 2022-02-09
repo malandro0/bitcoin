f6535f78c8c238bec7476b2fe438689839482aff
w0xlt <94266259+w0xlt@users.noreply.github.com>
2022-02-08 22:41:35 -0300

    rpc: allow dumptxoutset to dump human-readable data
    Co-authored-by: Shashwat Vangani <shaavan.github@gmail.com>
    Co-authored-by: Luke Dashjr <luke-jr+git@utopios.org>

--- src/test/util/chainstate.h
+++ src/test/util/chainstate.h
@@ -34,6 +34,7 @@ CreateAndActivateUTXOSnapshot(node::NodeContext& node, const fs::path root, F ma
     AutoFile auto_outfile{outfile};
 
     UniValue result = CreateUTXOSnapshot(
+        false, false, Span<std::byte>(), {},
         node, node.chainman->ActiveChainstate(), auto_outfile, snapshot_path, snapshot_path);
     LogPrintf(
         "Wrote UTXO snapshot to %s: %s", fs::PathToString(snapshot_path.make_preferred()), result.write());
