4c8180515f179c88e645d99710ddb382c98a9702
Jon Atack <jon@atack.com>
2022-03-31 01:38:42 +0200

    rpc: decodepsbt fixups
    Github-Pull: #24718
    Rebased-From: 758b7cf0fe8927f2a297124aa2e2d683a204228e

--- src/rpc/rawtransaction.cpp
+++ src/rpc/rawtransaction.cpp
@@ -693,10 +693,10 @@ const RPCResult decodepsbt_inputs{
                 {RPCResult::Type::OBJ, "scriptPubKey", "",
                 {
                     {RPCResult::Type::STR, "asm", "Disassembly of the public key script"},
-                    {RPCResult::Type::STR, "desc", "Inferred descriptor for the output"},
+                    {RPCResult::Type::STR, "desc", "Inferred descriptor for the script"},
                     {RPCResult::Type::STR_HEX, "hex", "The raw public key script bytes, hex-encoded"},
-                    {RPCResult::Type::STR, "type", "The type, eg 'pubkeyhash'"},
                     {RPCResult::Type::STR, "address", /*optional=*/true, "The Bitcoin address (only if a well-defined address exists)"},
+                    {RPCResult::Type::STR, "type", "The type (one of: " + GetAllOutputTypes() + ")"},
                 }},
             }},
             {RPCResult::Type::OBJ_DYN, "partial_signatures", /*optional=*/true, "",
@@ -708,13 +708,13 @@ const RPCResult decodepsbt_inputs{
             {
                 {RPCResult::Type::STR, "asm", "Disassembly of the redeem script"},
                 {RPCResult::Type::STR_HEX, "hex", "The raw redeem script bytes, hex-encoded"},
-                {RPCResult::Type::STR, "type", "The type, eg 'pubkeyhash'"},
+                {RPCResult::Type::STR, "type", "The type (one of: " + GetAllOutputTypes() + ")"},
             }},
             {RPCResult::Type::OBJ, "witness_script", /*optional=*/true, "",
             {
                 {RPCResult::Type::STR, "asm", "Disassembly of the witness script"},
                 {RPCResult::Type::STR_HEX, "hex", "The raw witness script bytes, hex-encoded"},
-                {RPCResult::Type::STR, "type", "The type, eg 'pubkeyhash'"},
+                {RPCResult::Type::STR, "type", "The type (one of: " + GetAllOutputTypes() + ")"},
             }},
             {RPCResult::Type::ARR, "bip32_derivs", /*optional=*/true, "",
             {
@@ -814,13 +814,13 @@ const RPCResult decodepsbt_outputs{
             {
                 {RPCResult::Type::STR, "asm", "Disassembly of the redeem script"},
                 {RPCResult::Type::STR_HEX, "hex", "The raw redeem script bytes, hex-encoded"},
-                {RPCResult::Type::STR, "type", "The type, eg 'pubkeyhash'"},
+                {RPCResult::Type::STR, "type", "The type (one of: " + GetAllOutputTypes() + ")"},
             }},
             {RPCResult::Type::OBJ, "witness_script", /*optional=*/true, "",
             {
                 {RPCResult::Type::STR, "asm", "Disassembly of the witness script"},
                 {RPCResult::Type::STR_HEX, "hex", "The raw witness script bytes, hex-encoded"},
-                {RPCResult::Type::STR, "type", "The type, eg 'pubkeyhash'"},
+                {RPCResult::Type::STR, "type", "The type (one of: " + GetAllOutputTypes() + ")"},
             }},
             {RPCResult::Type::ARR, "bip32_derivs", /*optional=*/true, "",
             {
