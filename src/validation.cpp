bitcoincore-dev/bitcoin-2001-863168-294116-d812cf1189

txmempool: Store pointers to transactions claiming SPKs

src/validation.cpp
@@ -733,9 +733,9 @@ bool AcceptToMemoryPoolWorker(CTxMemPool& pool, CValidationState& state, const C
                         return state.DoS(0, false, REJECT_NONSTANDARD, "txn-spk-reused-change");
                     }
                 }
-                mssit = pool.mapUsedSPK.find(hashSPK);
-                if (mssit != pool.mapUsedSPK.end()) {
-                    if (mssit->second & MSS_SPENT) {
+                const auto& SPKit = pool.mapUsedSPK.find(hashSPK);
+                if (SPKit != pool.mapUsedSPK.end()) {
+                    if (SPKit->second.second /* Spent */) {
                         return state.DoS(0, false, REJECT_CONFLICT, "txn-mempool-spk-reused-spend");
                     }
                 }

malandro0/bitcoin

src/validation.cpp
@@ -858,9 +858,9 @@ bool MemPoolAccept::PreChecks(ATMPArgs& args, Workspace& ws)
                     return state.Invalid(TxValidationResult::TX_MEMPOOL_POLICY, "txn-spk-reused-change");
                 }
             }
-            mssit = m_pool.mapUsedSPK.find(hashSPK);
-            if (mssit != m_pool.mapUsedSPK.end()) {
-                if (mssit->second & MSS_SPENT) {
+            const auto& SPKit = m_pool.mapUsedSPK.find(hashSPK);
+            if (SPKit != m_pool.mapUsedSPK.end()) {
+                if (SPKit->second.second /* Spent */) {
                     return state.Invalid(TxValidationResult::TX_MEMPOOL_POLICY, "txn-mempool-spk-reused-spend");
                 }
             }